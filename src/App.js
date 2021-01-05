57e08729dc05a620bba02fe28d5d2a1a7384b8e8
Kasturi <kasturivanjankar@gmail.com>
2021-01-05 14:08:24 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Login from './utils/Containers/Login/Login'
+import Footer from './utils/Components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Login />
-      
+      <Footer />
     </div>
   );
 }

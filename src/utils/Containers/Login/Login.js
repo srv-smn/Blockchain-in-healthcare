529d77f33eda2d0945adfcc0501465cd9623aad1
sourav <saurav2fly@gmail.com>
2021-01-05 14:07:12 +0530

    added background in login

--- src/utils/Containers/Login/Login.js
+++ src/utils/Containers/Login/Login.js
@@ -1,9 +1,44 @@
 import React,{Component} from 'react';
-
+import Classes from './Login.module.css'
+import Doctor from './../../../assets/doctor.png'
+import Patient from './../../../assets/patient.png'
+import Admin from './../../../assets/admin.png'
+import Back from './../../../assets/background.jpg'
 
 class Login extends Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return(
+      <div className={Classes.center} style={{ backgroundImage: `url(${Back})` }}>
+        <form>
+        <button>
+      <div className={Classes.container}>
+        <img src={Doctor} alt="Doctor" className={Classes.image} />
+        <div className={Classes.overlay}>
+          <div className={Classes.text}>Doctor</div>
+        </div>
+      </div>
+      </button>
+      <button>
+      <div className={Classes.container}>
+        <img src={Patient} alt="Doctor" className={Classes.image} />
+        <div className={Classes.overlay}>
+          <div className={Classes.text}>Patient</div>
+        </div>
+      </div>
+      </button>
+
+      <button>
+      <div className={Classes.container}>
+        <img src={Admin} alt="Doctor" className={Classes.image} />
+        <div className={Classes.overlay}>
+          <div className={Classes.text}>Admin</div>
+        </div>
+      </div>
+      </button>
+      </form>
+      </div>
+    )
+    
   }
 }
 

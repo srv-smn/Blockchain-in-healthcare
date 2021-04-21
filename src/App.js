3a014cb52fb25aa5755ee5fd0c282a92b41f9102
Kasturi <kasturivanjankar@gmail.com>
2021-04-21 11:18:54 +0530

    Registration form and search page added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import './App.css';
 import Login from './utils/Containers/Login/Login'
 import Footer from './utils/Components/Footer/Footer'
 import Header from './utils/Components/Header/Header';
-import LoginPage from './utils/Containers/LoginPage/UserReg';
-import FormExampleFieldControlId from './utils/Containers/LoginPage/UserReg';
+import DoctorLogin from './utils/Containers/DoctorLogin/doctorlogin';
+import PatientLogin from './utils/Containers/PatientLogin/patientlogin';
+import CardExampleFluid from './utils/Containers/UserReg/UserReg';
 import {Route} from 'react-router-dom'
-import NavBar from './utils/Components/NavBar/NavBar';
-
+import './App.css';
+import SearchDoc from './utils/Containers/SearchDoc/SearchDoc';
+import SearchPatient from './utils/Containers/SearchPatient/SearchPatient';
 
 function App() {
   return (
     <div className="App">
+    <searchDoc />
     <Header />
-    <NavBar />
     <Route path="/" exact component={Login} />
-    <Route path="/newform" component={FormExampleFieldControlId} />
+    <Route path="/newform" component={CardExampleFluid} />
     <Route path="/home" component={Login} />
+    <Route path="/doctorlogin" component={DoctorLogin} />
+    <Route path="/patientlogin" component={PatientLogin} />
+
     <Footer />
+    <br />
+    <SearchDoc />
+    <br />
+    <SearchPatient />
     </div>
+    
   );
 }
 

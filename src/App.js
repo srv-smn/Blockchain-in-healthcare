fc4a8354b04427356593c05af234d3ccd49c142c
sourav <saurav2fly@gmail.com>
2021-04-27 07:36:58 +0530

    added basic changes eth

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import DoctorLogin from './utils/Containers/DoctorLogin/doctorlogin';
 import PatientLogin from './utils/Containers/PatientLogin/patientlogin';
 import CardExampleFluid from './utils/Containers/UserReg/UserReg';
 import {Route} from 'react-router-dom'
+<<<<<<< HEAD
 
+=======
+import NavBar from './utils/Components/NavBar/Navbar';
+>>>>>>> Merge branch 'main' of https://github.com/srv-smn/Blockchain-in-healthcare into main
 import './App.css';
 import SearchDoc from './utils/Containers/SearchDoc/SearchDoc';
 import SearchPatient from './utils/Containers/SearchPatient/SearchPatient';
